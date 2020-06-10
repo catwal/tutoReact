be45df2bdeb6ea57dde6bface94a6936b03a604e
Catherine WALLEZ <catherine@meteor.st>
2020-06-10 17:40:57 +0200

    change stateless component to class component with state

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,10 +1,30 @@
 import React from 'react';
-//fonction
-const Form = () => {
+
+class Form extends React.Component {
+    state = {
+        name: '',
+        quantity: 0
+    };
+
+    render() {
+        return (
+            <div>
+                <h3>Ajouter des articles à acheter</h3>
+                <form>
+                    <input type="number" placeholder="quantité"/>
+                    <input type="text" placeholder="nom de l'article"/>
+                    <button type="submit">Ajouter</button>
+                </form>
+            </div>
+        );
+    }
+}
+
+/*const Form = () => {
 
     return (
       <div>Form Component</div>
     );
-};
+};*/
 
 export default Form;
