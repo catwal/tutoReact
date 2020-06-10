1f1c35f73e3ffdd2cfa6471459c3f972ad1f90f8
Catherine WALLEZ <catherine@meteor.st>
2020-06-10 17:41:19 +0200

    import class in app.js

--- src/App.js
+++ src/App.js
@@ -2,18 +2,19 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import ItemList from './components/ItemList';
-import Form     from './components/Form';
+import Form from './components/Form';
 
 function App() {
   return (
     <div className="App">
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-
       </header>
+
         <h3>Liste de courses</h3>
 
         <Form />
